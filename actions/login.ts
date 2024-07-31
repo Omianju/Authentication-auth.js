dc5167978fddbf4cedafdbe0f21461ac4fa1bf59
Omianju <devansh1413@gmail.com>
2024-07-31 12:40:15 -0700

    email verification feature added

--- actions/login.ts
+++ actions/login.ts
@@ -10,6 +10,7 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 import { AuthError } from "next-auth"
 import { getUserByEmail } from "@/data/user"
 import { generateVerificationToken } from "@/lib/tokens"
+import { sendVerificationEmail } from "@/lib/mail"
 
 export const login = async (values : z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
@@ -25,7 +26,13 @@ export const login = async (values : z.infer<typeof LoginSchema>) => {
     }
 
     if (!existingUser.emailVerified) {
-        const verificationToken = generateVerificationToken(existingUser.email)
+        const verificationToken = await generateVerificationToken(existingUser.email)
+
+        await sendVerificationEmail(
+            verificationToken.email,
+            verificationToken.token
+        )
+
         return { success : "Confirmation Email sent!" }
     }
 
---
+++ components/auth/verification-form.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import { CardWrapper } from "./card-wrapper"
+import { BeatLoader } from "react-spinners"
+import { useCallback, useEffect, useState } from "react"
+import { newVerification } from "@/actions/newVerification"
+import { FormSuccess } from "../form-success"
+import { FormError } from "../form-error"
+
+
+export const NewVerificationForm = () => {
+    const [success, setSuccess] = useState<string | undefined>("")
+    const [error, setError] = useState<string | undefined>("")
+    const searchParams = useSearchParams()
+    const token = searchParams.get("token")
+
+    const onSubmit = useCallback(()=>{
+        if (!token) return setError("Token is missing!")
+        
+        newVerification(token)
+        .then((data)=>{
+            setSuccess(data?.success)
+            setError(data?.error)
+        })
+        .catch(()=>{
+            setError("Something went wrong!")
+        })
+    }, [token, error, success])
+
+    useEffect(()=>{
+        onSubmit()
+    },[onSubmit])
+
+    return (
+        <CardWrapper
+         headerLabel="Confirming your verification!"
+         backButtonLabel="Back to login."
+         backButtonHref="/auth/login"
+         >
+        <div className="w-full flex items-center justify-center " >    
+           
+            { !error && !success && (
+                <BeatLoader />
+            ) }
+            
+            <FormSuccess message={success} />
+            <FormError message={error} />
+        </div>
+        </CardWrapper>
+    )
+}
